7990a675b04d9f95de798ad9285ff4a34a0efd70
Origina-sudo <arthurba8585@gmail.com>
2024-10-08 21:30:58 +0000

    updates

--- EcommerceAPI/DjangoAPIapp/admin.py
+++ EcommerceAPI/DjangoAPIapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Category, Book, Product, Cart
-# Register your models here.
+# Register your models here..
 
 admin.site.register(Category)
 admin.site.register(Book)
